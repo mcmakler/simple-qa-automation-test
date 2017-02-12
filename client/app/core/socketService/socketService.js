230a8ccba36b4df9bcee96abb2a305126fe1171c
saibs <sai.b.satchitanand@gmail.com>
2017-02-13 00:18:31 +0100

    Fixed linting bugs

--- client/app/core/socketService/socketService.js
+++ client/app/core/socketService/socketService.js
@@ -5,8 +5,8 @@ export class SocketService {
 		'ngInject';
 
 		this.socket = socketFactory({
-				ioSocket: io('http://localhost:3000'),
-			});
+			ioSocket: io('http://localhost:3000'),
+		});
 
 		this.userId = $window.sessionStorage.getItem('socket:userId') || this.generateUserId();
 		$window.sessionStorage.setItem('socket:userId', this.userId);
@@ -17,15 +17,15 @@ export class SocketService {
 	generateUserId() {
 		let d = new Date().getTime();
 
-		if(window.performance && typeof window.performance.now === "function"){
+		if (window.performance && typeof window.performance.now === 'function') {
 			d += window.performance.now(); //use high-precision timer if available
 		}
 
 		const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-			const r = (d + Math.random()*16)%16 | 0;
+			const r = (d + Math.random() * 16) % 16 | 0;
 
-			d = Math.floor(d/16);
-			return (c === 'x' ? r : (r&0x3|0x8)).toString(16);
+			d = Math.floor(d / 16);
+			return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
 		});
 
 		return uuid;
